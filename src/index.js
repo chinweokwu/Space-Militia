b9058f5e4d5f757db1c0c7de9d1ce51350ccb27d
chinweokwu <paulcmorah@gmail.com>
2021-01-20 20:39:45 +0100

    added title page and progress bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import Phaser from 'phaser';
+import './assets/styles.css';
 import config from './config';
-import game from './scenes/game-scene';
+import GameScene from './scenes/GameScene';
+import BootScene from './scenes/BootScene';
+import PreloaderScene from './scenes/PreloaderScene';
+import TitleScene from './scenes/TitleScene';
+import instructions from './scenes/instructionScene';
+import leaderboard from './scenes/leaderboardScene';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
-    this.scene.add('Game', game);
-    this.scene.start('Game');
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Instructions', instructions);
+    this.scene.add('Leaderboard', leaderboard);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Boot');
   }
 }
 
---
+++ src/scenes/leaderboardScene.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser';
+
+export default class LeaderboardScene extends Phaser.Scene {
+  constructor() {
+    super('Leaderboard');
+  }
+
+  create() {
+   
+  }
+}
